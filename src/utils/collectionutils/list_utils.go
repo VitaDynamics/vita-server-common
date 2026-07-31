15a199768141d3e517077c4c5ecf43f5a19e4572
sunguiyong <guiyong.sun@vbot.cn>
2026-07-31 14:24:04 +0800

    add collection utils method

--- src/utils/collectionutils/list_utils.go
+++ src/utils/collectionutils/list_utils.go
@@ -27,3 +27,45 @@ func ListContains(list []string, value string, logCtx map[string]interface{}) bo
 
 	return false
 }
+
+func ListContainsInt(list []int, value int, logCtx map[string]interface{}) bool {
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.WithFields(logCtx).Errorf("collectionutils ListContainsInt panic: %v", r)
+		}
+	}()
+	for _, item := range list {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
+
+func ListContainsInt64(list []int64, value int64, logCtx map[string]interface{}) bool {
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.WithFields(logCtx).Errorf("collectionutils ListContainsInt64 panic: %v", r)
+		}
+	}()
+	for _, item := range list {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
+
+func ListContainsFloat64(list []float64, value float64, logCtx map[string]interface{}) bool {
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.WithFields(logCtx).Errorf("collectionutils ListContainsFloat64 panic: %v", r)
+		}
+	}()
+	for _, item := range list {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
